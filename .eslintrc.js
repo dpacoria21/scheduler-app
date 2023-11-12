5abe17b32651d966af6321d2f8b3c64f6621e17e
Diego Pacori <dpacoria@unsa.edu.pe>
2023-11-11 20:21:00 -0500

    Se dio inicio al proyecto scheduler-app

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    root: true,
+    extends: '@react-native',
+    'rules': {
+        'indent': [
+            'error',
+            4,
+        ],
+        'linebreak-style': 0,
+        'quotes': [
+            'error',
+            'single',
+        ],
+        'semi': [
+            'error',
+            'always',
+        ],
+        'prettier/prettier': 0,
+        '@typescript-eslint/no-empty-interface': 'off',
+    },
+};
